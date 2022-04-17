da6da969dd349c8b37af33ea4dc3fcd1a65e1765
mariapanizo <mariapanizo92@gmail.com>
2022-04-16 23:22:00 -0400

    updated icon and removed unnecessary code

--- src/Script.js
+++ src/Script.js
@@ -41,8 +41,14 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-  getForecast(response.data.coord);
-}
+    iconElement.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+    getForecast(response.data.coord);
+  }
 
 function searchCity(city) {
   let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
